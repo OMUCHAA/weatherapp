31efa7cceda95e2abf758065a46d51c9d21dfdc9
OMUCHAA <shemomu42@gmail.com>
2025-04-22 13:07:53 +0300

    Adding a functionality where, when the search icon is clicked, the value inside the input field is captured and passed through the function and runs the weather conditions of the specified city value.

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const apiKey = 'e0457806ab1860f13a874a82919663b3';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=nairobi&units=metric';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?&units=metric&q=';
+const searchBtn = document.querySelector('.search button');
+const searchBox = document.querySelector('.search input');
 
-async function checkWeather() {
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city +  `&appid=${apiKey}`);
     let data = await response.json();
 
     document.querySelector('.city').innerHTML = data.name;
@@ -11,5 +13,7 @@ async function checkWeather() {
     document.querySelector('.wind').innerHTML = data.wind.speed + ' Km/h';
     console.log(data);
 }
-checkWeather();
 
+searchBtn.addEventListener('click',() => {
+    checkWeather(searchBox.value);
+})
